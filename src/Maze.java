8271c60b415e5173acfd1ffba7dcd450646f36c5
connorfurby <cafurby@stu.naperville203.org>
2023-10-27 12:45:33 -0500

    Maze Skeleton

---
+++ src/Maze.java
@@ -0,0 +1,44 @@
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Maze {
+
+    private Square[][] maze;
+    int numRows, numCols;
+    public Maze()
+    {
+
+    }
+
+    boolean loadMaze(String fname)
+    {
+       
+    }
+    
+    public ArrayList<Square> getNeighbors(Square sq)
+    {
+        
+    }
+
+    public Square getStart()
+    {
+
+    }
+
+    public Square getFinish()
+    {
+
+    }
+
+    public void reset()
+    {
+
+    }
+
+    public String toString()
+    {
+
+    }
+}
