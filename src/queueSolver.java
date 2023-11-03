aa3a2cfa73a18cc2b7b7435791f44213edc3fdc7
Samanthap3 <span@stu.naperville203.org>
2023-11-03 13:18:44 -0500

    work on queueSolver
    might have to edit later - also change worklist to be all squares

---
+++ src/queueSolver.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class queueSolver extends MazeSolver{
+    
+    public queueSolver(Maze maze) {
+        super(maze);
+        //TODO Auto-generated constructor stub
+    }
+
+    MyQueue <Square> queue = new MyQueue();
+    
+    public void makeEmpty()
+    {
+
+    }
+    public boolean isEmpty()
+    {
+
+    }
+    public void add(Square sq)
+    {
+
+    }
+    public Square next()
+    {
+        
+    }
+}
